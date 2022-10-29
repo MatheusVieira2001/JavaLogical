efbeb27a3f1ea2af6966d40d7c126c8b41981536
Matheus Vieira <108036687+MatheusVieira2001@users.noreply.github.com>
2022-10-29 20:12:56 -0300

    Update Calculator.java

--- Calculator.java
+++ Calculator.java
@@ -1,5 +1,16 @@
 package util;
 public class Calculator {
+  /* 
+metodos estaticos para que não precise instanciar o objeto na classe principal 
+public static final double PI = 3.14159;
+public static double circumference(double radius) {
+return 2.0 * PI * radius;
+}
+public static double volume(double radius) {
+return 4.0 * PI * radius * radius * radius / 3.0;
+}*/
+  
+  
 public final double PI = 3.14159;
 public double circumference(double radius) {
 return 2.0 * PI * radius;
