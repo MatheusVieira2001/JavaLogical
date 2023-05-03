e15742eb7b105765bd953dcb6c6b42d8bb56511b
Matheus Vieira <108036687+MatheusVieira2001@users.noreply.github.com>
2023-05-02 22:04:50 -0300

    Add files via upload

---
+++ Controle de Fluxo - Desafio/ParametrosInvalidosException.java
@@ -0,0 +1,11 @@
+package com.learning.dio;
+
+/**
+ *
+ * @author Matheus
+ */
+public class ParametrosInvalidosException extends Exception{
+    public ParametrosInvalidosException(String mensagem) {
+		super(mensagem);
+	}
+}
